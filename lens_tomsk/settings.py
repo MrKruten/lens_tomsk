10401b76bb16721e3cea386d56e3eb8b1b5bbe49
Kepby1608 <Cemen.Luzin@yandex.ru>
2022-11-19 00:12:20 +0700

    try fix: static files

--- lens_tomsk/settings.py
+++ lens_tomsk/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
