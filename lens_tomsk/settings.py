2055b3e9411f7fcaaf8914dbe02de02317100e2a
Kepby1608 <Cemen.Luzin@yandex.ru>
2022-11-19 00:23:22 +0700

    try fix: static files

--- lens_tomsk/settings.py
+++ lens_tomsk/settings.py
@@ -137,9 +137,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATICFILES_DIRS = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
